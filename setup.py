384152ae43859ffb43ddf635ecdc17c9e48ca76a
Ryan Jung <GradysGhost@gmail.com>
2014-03-21 10:30:58 -0500

    Adding readme to release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name='PyMoJo',
-  version='0.1.0',
+  version='0.2.1',
   author='Ryan Jung',
   author_email='gradysghost@gmail.com',
   packages=['pymojo'],
