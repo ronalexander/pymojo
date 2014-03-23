979953facfd4fdb1fa107894c2197da9634f6c5c
Ryan Jung <gradysghost@gmail.com>
2014-03-23 11:28:34 -0500

    Changes for publication

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name='PyMoJo',
-  version='0.2.1',
+  version='0.2.2',
   author='Ryan Jung',
   author_email='gradysghost@gmail.com',
   packages=['pymojo'],
