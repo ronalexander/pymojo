b6f44d77deb6b9937f563d4547904c785f3e6a3a
Ryan Jung <gradysghost@gmail.com>
2014-03-24 09:51:53 -0500

    Repackaging

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
   name='PyMoJo',
-  version='0.2.2',
+  version='0.2.4',
   author='Ryan Jung',
   author_email='gradysghost@gmail.com',
   packages=['pymojo'],
   url='https://github.com/GradysGhost/pymojo',
   license='LICENSE',
   description='PyJoJo client library',
-  long_description=open('README.md').read(),
+  long_description=open('README.txt').read(),
   install_requires=[
     'requests == 1.1.0'
   ]
