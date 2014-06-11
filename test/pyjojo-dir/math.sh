1620a1ba2c6ccd5a06667fc77aca29c170574bf3
Ryan Jung <gradysghost@gmail.com>
2014-06-10 23:34:28 -0500

    Adding math script to test stuff

---
+++ test/pyjojo-dir/math.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# -- jojo --
+# description: Solves simple math expressions
+# param: expr - The expression to solve
+# tags: math, solve
+# http_method: post
+# output: split
+# -- jojo --
+
+solution=`echo $EXPR | bc`
+echo "$EXPR = $solution"
+echo "jojo_return_value solution=$solution"
+echo "jojo_return_value expr=$EXPR"
+# echo $solution
