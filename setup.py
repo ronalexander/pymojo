44dd0d4a07ef1444014e8c5919ec01fa72c36fb6
Ryan Jung <GradysGhost@gmail.com>
2014-07-25 15:47:46 -0500

    Bugfixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name='PyMoJo',
-  version='0.8.3',
+  version='0.8.4',
   author='Ryan Jung',
   author_email='gradysghost@gmail.com',
   packages=['pymojo'],
