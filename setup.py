7b605b1340c44954f899ea17dcf9f57ad85a0d0b
Ryan Jung <GradysGhost@gmail.com>
2014-09-18 16:41:47 -0500

    Made changes to packaging code

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
+"""This module sets up the Mojo CLI app and libraries
+"""
+
 from distutils.core import setup
 
 setup(
-  name='PyMoJo',
-  version='0.8.5',
-  author='Ryan Jung',
-  author_email='gradysghost@gmail.com',
-  packages=['pymojo'],
-  url='https://github.com/GradysGhost/pymojo',
-  license='LICENSE',
-  description='PyJoJo client library',
-  long_description=open('README.md').read(),
-  install_requires=[
-    'requests == 1.1.0',
-    'PyYAML == 3.10'
-  ],
-  entry_points={
-    'console_scripts' : [
-      'mojo = pymojo.cli_bin:main'
-    ]
-  }
+    name='PyMoJo',
+    version='0.9',
+    author='Ryan Jung',
+    author_email='gradysghost@gmail.com',
+    packages=['pymojo'],
+    url='https://github.com/GradysGhost/pymojo',
+    license='LICENSE',
+    description='PyJoJo client library',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'requests == 2.4.1'
+        'PyYAML == 3.11'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mojo = pymojo.cli_bin:main'
+        ]
+    }
 )
